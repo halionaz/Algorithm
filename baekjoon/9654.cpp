61d25f5372794820577642a7a7df50172fc56e0e
haSaJa <samhsj@naver.com>
2022-04-12 22:39:47 +0900

    delete exec file

---
+++ baekjoon/9654.cpp
@@ -0,0 +1,14 @@
+// 나부 함대 데이터
+// 구현
+
+#include <iostream>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cout << "SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21        \nJ-Type 327     Light Combat   Unlimited  1         \nNX Cruiser     Medium Fighter Limited    18        \nN1 Starfighter Medium Fighter Unlimited  25        \nRoyal Cruiser  Light Combat   Limited    4         \n";
+
+    return 0;
+}
