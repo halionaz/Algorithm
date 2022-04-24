38a4fc27be9f4953da6f9a6d327d978d4072c363
halionaz <samhsj@naver.com>
2022-04-23 20:52:15 -0400

    20254

---
+++ baekjoon/20254.cpp
@@ -0,0 +1,19 @@
+// Site Score
+// 사칙연산
+
+#include <iostream>
+
+int Ur, Tr, Uo, To;
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> Ur >> Tr >> Uo >> To;
+
+    std::cout << 56*Ur + 24*Tr + 14*Uo + 6*To << '\n';
+
+    return 0;
+    
+}
