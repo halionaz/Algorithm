30f33bff3459fc344b73b9643bb12b314a2580f5
halionaz <samhsj@naver.com>
2022-04-25 07:33:20 -0400

    23234

---
+++ baekjoon/23234.cpp
@@ -0,0 +1,14 @@
+// The World Responds
+// 구현
+
+#include <iostream>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cout << "The world says hello!\n";
+
+    return 0;
+}
