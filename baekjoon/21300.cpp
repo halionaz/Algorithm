f0813a2ef4e98c3169b82402897fff229d1dec45
halionaz <samhsj@naver.com>
2022-04-29 04:06:17 -0400

    21300 & 25083

---
+++ baekjoon/21300.cpp
@@ -0,0 +1,17 @@
+// Bottle Return
+// 사칙연산
+
+#include <iostream>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    int a, b, c, d, e, f;
+
+    std::cin >> a >> b >> c >> d >> e >> f;
+    std::cout << (a+b+c+d+e+f)*5 << '\n';
+
+    return 0;
+}
---
+++ baekjoon/25083.cpp
@@ -0,0 +1,14 @@
+// 새싹
+// 구현 
+
+#include <iostream>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cout << "         ,r'\"7\nr`-_   ,'  ,/\n \\. \". L_r'\n   `~\\/\n      |\n      |" << '\n';
+
+    return 0;
+}
