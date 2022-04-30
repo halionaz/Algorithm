59d98b12aa5b65f48d67af6eb53aa5865d4a6893
halionaz <samhsj@naver.com>
2022-04-29 20:54:43 -0400

    8871

---
+++ baekjoon/8871.cpp
@@ -0,0 +1,18 @@
+// Zadanie próbne 2
+// 사칙연산
+
+#include <iostream>
+
+int main() {
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    int N;
+
+    std::cin >> N;
+
+    std::cout << (N + 1) * 2 << ' ' << (N + 1) * 3 << '\n';
+
+    return 0;
+}
