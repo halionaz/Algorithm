7f9b3fbe9c8122106473da800ef80491d63bdc88
halionaz <samhsj@naver.com>
2022-05-06 23:07:36 -0400

    15680

---
+++ baekjoon/15680.cpp
@@ -0,0 +1,19 @@
+// 연세대학교
+// 구현 
+
+#include <iostream>
+
+int main(){
+
+    int N;
+
+    std::cin >> N;
+
+    if(N){
+        std::cout << "Leading the Way to the Future\n";
+    } else {
+        std::cout << "YONSEI\n";
+    }
+
+    return 0;
+}
