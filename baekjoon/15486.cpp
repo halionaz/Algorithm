6a614de5272ff4771c893643c2e317c5431926af
halionaz <samhsj@naver.com>
2022-05-19 07:13:59 -0400

    15486

---
+++ baekjoon/15486.cpp
@@ -0,0 +1,34 @@
+// 퇴사 2
+// 다이나믹 프로그래밍
+
+#include <iostream>
+#include <algorithm>
+
+int N;
+int dp[1500002];
+int T[1500002];
+int P[1500002];
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> N;
+
+    for(int i = 1; i <= N; i++){
+        std::cin >> T[i] >> P[i];
+    }
+
+    for(int i = N; i > 0; i--){
+        if(i + T[i] > N + 1){
+            dp[i] = dp[i+1];
+        } else {
+            dp[i] = std::max(dp[i+1], dp[i+T[i]] + P[i]);
+        }
+    }
+
+    std::cout << dp[1] << '\n';
+
+    return 0;
+}
