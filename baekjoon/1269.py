3b129e4c9815b75dd306127a00d5a0d1fee3d378
halionaz <samhsj@naver.com>
2022-05-21 23:17:00 -0400

    1269

---
+++ baekjoon/1269.py
@@ -0,0 +1,12 @@
+# 대칭 차집합
+# 자료 구조 
+
+# 집합과 맵 사용
+
+import sys
+input = sys.stdin.readline
+_ = input()
+A = set(map(int, input().split()))
+B = set(map(int, input().split()))
+
+print(len(A-B) + len(B-A))
