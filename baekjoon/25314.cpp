ad562bd7ace367bdcff229fefb3cf8522b070aa8
halionaz <samhsj@naver.com>
2022-07-02 20:02:28 -0400

    ✨ 25314

---
+++ baekjoon/25314.cpp
@@ -0,0 +1,21 @@
+// 코딩은 체육과목 입니다
+
+#include <iostream>
+
+int N;
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> N;
+
+    for(int i = 0; i < N/4; i++){
+        std::cout << "long ";
+    }
+
+    std::cout << "int\n";
+
+    return 0;
+}
