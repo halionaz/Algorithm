c0e4e5e4813c4cd9f2246a3c26da16267beba1fa
halionaz <samhsj@naver.com>
2022-08-15 09:07:22 -0400

    ✨ 2743

---
+++ baekjoon/2743.cpp
@@ -0,0 +1,19 @@
+// 단어 길이 재기
+// 구현 & 문자열
+
+#include <iostream>
+#include <string.h>
+
+std::string S;
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> S;
+
+    std::cout << S.length() << '\n';
+
+    return 0;
+}
