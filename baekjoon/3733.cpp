b06221f08728588ad71e1dabddb417141b7def8c
halionaz <samhsj@naver.com>
2022-08-20 22:30:02 -0400

    ✨ 3733

---
+++ baekjoon/3733.cpp
@@ -0,0 +1,21 @@
+// Shares
+// 사칙연산
+
+// inf_input
+
+#include <iostream>
+#include <cstdio>
+
+int N, S;
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    while (~scanf("%d %d", &N, &S)) {
+        std::cout << S / (N+1) << '\n';
+    }
+
+    return 0;
+}
