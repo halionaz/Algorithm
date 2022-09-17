1166d675e4926d5a25ed816f315d6777cab9536c
halionaz <samhsj@naver.com>
2022-09-17 07:26:39 -0400

    🏷️ 1753 to 1759 Code Review

--- baekjoon/1753.py
+++ baekjoon/1753.py
@@ -9,25 +9,32 @@ import sys
 import heapq
 input = sys.stdin.readline
 inf = 9999999999999999
+
 V,E = map(int,input().split())
 K = int(input())
 line = [[] for _ in range(V+1)]
 for _ in range(E) :
     u,v,w = map(int,input().split())
     line[u].append((v,w))
+
 memo = [inf]*(V+1)
 heap = []
 memo[K] = 0
+
+# 시작 정점
 heapq.heappush(heap,(0,K))
 while len(heap) :
     dist,index = heapq.heappop(heap)
     # 서로 다른 두 정점 사이에 간선이 여러개 있을 수 있으므로, 이미 처리된 간선이면 pass
-    if not memo[index] < dist :
+    if memo[index] < dist :
+        continue
+    else :
         for i,weight in line[index] :
             nextDist = dist + weight
             if nextDist < memo[i] :
                 memo[i] = nextDist
                 heapq.heappush(heap,(nextDist,i))
+
 for i in range(1,V+1) :
     if memo[i] == inf :
         print('INF')
--- baekjoon/1759.py
+++ baekjoon/1759.py
@@ -5,28 +5,37 @@ L,C = map(int,input().split())
 strList = list(input().rstrip().split())
 strList.sort()
 visited = [0]*C
-vowel = []
+
 def dfs(depth,n) :
     if depth == L-1 :
         vowel = 0
         conso = 0
         ans = ''
         for i in range(C) :
+
             if visited[i] :
                 ans += strList[i]
+
+                # 자음 & 모음 개수 카운팅을 통한 유효성 판단
                 if strList[i] == 'a' or strList[i] == 'e' or strList[i] == 'i' or strList[i] == 'o' or strList[i] == 'u' :
                     vowel += 1
                 else :
                     conso += 1
+
         if vowel >= 1 and conso >= 2 :
             print(ans)
+
         return
+        
     else :
         for i in range(n+1,C) :
             if not visited[i] :
+                # 솔직히 어차피 정렬된 상태에서 선형적으로 탐색중이기에
+                # 당연히 i는 방문하지 않았을 것임. 따라서 별로 의미있는 조건문은 아님
                 visited[i] = 1
                 dfs(depth+1,i)
                 visited[i] = 0
+
 for i in range(C-L+1) :
     visited[i] = 1
     dfs(0,i)
