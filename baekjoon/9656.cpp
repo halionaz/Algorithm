f96cbb72918bfd32803d717c05ec03e943a888e7
halionaz <samhsj@naver.com>
2022-09-24 06:31:02 -0400

    ✨ 9656

---
+++ baekjoon/9656.cpp
@@ -0,0 +1,18 @@
+// 돌 게임 2
+// 게임 이론 & 수학
+
+#include <iostream>
+
+int N;
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> N;
+
+    std::cout << (N%2 == 0 ? "SK" : "CY") << "\n";
+
+    return 0;
+}
