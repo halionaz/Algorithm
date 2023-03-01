85332d118bdecbebd2955237961cef8099ad13a8
halionaz <samhsj@naver.com>
2023-02-28 22:53:13 -0500

    🏷️ 12126 Code Review

--- baekjoon/12126.cpp
+++ baekjoon/12126.cpp
@@ -1,10 +1,7 @@
 // Mearlin QA (Large)
 // 애드 혹 & 그리디 알고리즘
 
-// 공부가 더 필요함..
-// 비용만 들어가는 주문은 처음에 미리 다 시전해버리는 게 좋고 (답에 영향 없음)
-// 그 이후에 각 재료를 선택하는 순열에 따라 답을 계산하는 것 같은데 조금 더 연구가 필요함..!!
-// 거의 이해하지 못한 채 풀었음
+// 비용만 들어가는 주문은 처음에 미리 다 시전해버리는 게 좋음 (답에 영향 없음)
 
 #include <iostream>
 #include <vector>
@@ -23,6 +20,7 @@ int main(){
 
     for(int t = 1; t <= T; t++){
 
+        // 변수 초기화
         valuableSpells.clear();
         for(int i = 0; i < 100; i++){
             spells[i].clear();
@@ -31,6 +29,7 @@ int main(){
         std::cin >> N >> M;
 
         for(int i = 0; i < N; i++){
+            // 주문을 시전하여 얻을 수 있는 가치 계산
             int maxCost = 0;
             spells[i].resize(M);
             for(int j = 0; j < M; j++){
@@ -38,6 +37,7 @@ int main(){
                 maxCost = std::max(maxCost, spells[i][j]);
             }
             if(maxCost > 0){
+                // 주문을 통해 무언갈 얻을 수 있다면
                 valuableSpells.push_back(spells[i]);
             }
         }
@@ -48,27 +48,35 @@ int main(){
             continue;
         }
 
+        // 계산 전 버는게 없는 주문들은 다 시전해버렸다고 가정
+
         ans = 0;
 
         std::vector<int> permute(M);
+
         for(int i = 0; i < M; i++){
             permute[i] = i;
         }
 
         do{
-
+            // 어떤 재료를 마지막에 남기냐에 따라 주문의 순서는 정해질 수 밖에 없음
+            // 재료를 산정하는 순서를 순열을 통해 모든 경우를 돌려봄
+            // (재료 순서를 통해 어떤 재료를 마지막에 남길지 결정)
             int vals = 0;
+
             for(int i = 0; i < valuableSpells.size(); i++){
                 int val = 0;
                 int costs = 0;
 
+                // 재료를 계산하는 순서에 따라 각 시점 별 costs가 달라짐
                 for(int j = 0; j < M; j++){
                     costs += valuableSpells[i][permute[j]];
                     val = std::max(val, costs);
                 }
                 vals += val;
-
             }
+
+            // 정답 갱신
             ans = std::max(ans, vals);
 
         } while(std::next_permutation(permute.begin(), permute.end()));
