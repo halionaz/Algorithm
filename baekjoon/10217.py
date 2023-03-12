e64ea74eec97cf95717ffa1bb61258ab0ba186fc
halionaz <samhsj@naver.com>
2023-03-12 04:31:32 -0400

    🏷️ 10217 Code Review

--- baekjoon/10217.py
+++ baekjoon/10217.py
@@ -1,18 +1,24 @@
 # KCM Travel
 # 데이크스트라 & 다이나믹 프로그래밍
+# bottom-up
 
 import sys
 input = sys.stdin.readline
 INF = 99999999999999999999
+
 T = int(input())
+
 for _ in range(T) :
+
     N,M,K = map(int,input().split())
+
     ticket = [[] for _ in range(N+1)]
     for _ in range(K) :
         u,v,c,d = map(int,input().split())
         ticket[u].append((v,c,d))
+    
     dp = [[INF]*(M+1) for _ in range(N+1)]
-    #dp[i][j]=k 이면, i까지 가는 데, j이하의 돈을 쓴다면 k시간이 걸린다는 것
+    # dp[i][j] = k 이면, i까지 가는 데, j의 돈을 쓴다면 k시간이 걸린다는 것
     dp[1][0] = 0
     for i in range(M+1) :
         for here in range(1,N+1) :
@@ -22,7 +28,7 @@ for _ in range(T) :
                 lastTime = dp[here][i]
                 for end,cost,dist in ticket[here] :
                     if cost+i <= M :
-                        dp[end][cost+i] = min(dp[end][cost+i],lastTime+dist)
+                        dp[end][cost+i] = min(dp[end][cost+i], lastTime + dist)
     result = min(dp[N])
     if result == INF :
         print('Poor KCM')
