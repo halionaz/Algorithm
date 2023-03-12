3958de1e3f55be94a830ce3cba379678ecad0dff
halionaz <samhsj@naver.com>
2023-03-12 04:44:50 -0400

    🏷️ 4196 Code Review

--- baekjoon/4196.cpp
+++ baekjoon/4196.cpp
@@ -1,6 +1,15 @@
 // 도미노
 // 강한 연결 요소
 
+// scc의 개수를 구해주되,
+// 만약 scc에 묶여있진 않지만 그 scc 노드를 가리키는 외부 노드가 있다면
+// 그 친구를 넘어뜨리면 되므로
+// 단순 scc의 개수가 답은 아님
+
+// indegree의 개수가 0인 scc만 넘어뜨려주면
+// indegree가 존재하는 scc는 넘어진다는 사실을 알 수 있음
+// 즉 들어오는 간선이 없는 scc의 개수를 구하면 됨
+
 #include <iostream>
 #include <cstring>
 #include <vector>
@@ -48,6 +57,7 @@ int main(){
         
         // 초기화
         memset(visited,false,sizeof(visited));
+
         for(int i = 1; i <= N; i++){
             line[i].clear();
         }
@@ -65,7 +75,11 @@ int main(){
         }
 
         memset(visited,false,sizeof(visited));
+
         int ans = 0;
+
+        // 스택에 얹혀있는 순서대로 탐색하면
+        // indegree가 0인 노드부터 탐색 가능
         while(!S.empty()){
             int cur = S.top();
             S.pop();
