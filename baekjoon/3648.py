23cda0f5fab91197acc9b76bd48d38b6f170f7b1
halionaz <samhsj@naver.com>
2023-03-29 02:39:22 -0400

    🏷️ 3648 Code Review

--- baekjoon/3648.py
+++ baekjoon/3648.py
@@ -1,5 +1,6 @@
 # 아이돌
 # 2-sat & 강한 연결 요소
+# 코사라주 알고리즘
 
 # C++로 구현했을 때 계속 Out of Bound 런타임 에러가 뜸
 # 도저히 무엇이 문제인지 모르겠어서 Python으로 전환
@@ -24,38 +25,50 @@ def reverseDFS(cur) :
 
 while True :
     try :
+
         N,M = map(int, input().split());
+
+        # 데이터 준비
         line = [[] for _ in range(N*2+2)]
         reverseLine = [[] for _ in range(N*2+2)]
         visited = [False]*(N*2+2)
         scc = [-1]*(N*2+2)
         stck = []
         ind = -1
+
         for _ in range(M) :
             a,b = map(int, input().split())
+
             if a < 0 :
                 a = (-a)*2
             else :
                 a = a*2-1
+
             if b < 0 :
                 b = (-b)*2
             else :
                 b = b*2-1
+
             if a%2 :
                 notA = a+1
             else :
                 notA = a-1
+            
             if b%2 :
                 notB = b+1
             else :
                 notB = b-1
+
             line[notA].append(b)
             reverseLine[b].append(notA)
             line[notB].append(a)
             reverseLine[a].append(notB)
+        
         for i in range(1,2*N+1) :
             if not visited[i] :
                 DFS(i)
+        
+        # visited 초기화
         for i in range(0, len(visited)) :
             visited[i] = False
         
@@ -69,11 +82,13 @@ while True :
         ans = True
 
         if scc[1] <= scc[2] :
+            # 상근이가 탈락함
             ans = False
         
         if ans :
             for i in range(1,N+1) :
                 if scc[i*2-1] == scc[i*2] :
+                    # 모순 발생, 심사위원이 의심하게 됨
                     ans = False
                     break
         
