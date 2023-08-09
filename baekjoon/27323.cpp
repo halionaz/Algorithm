2ae365df9ab73c4f2f6cd798c60166004f89bc55
samhsj <samhsj@naver.com>
2023-08-09 05:06:36 -0400

    ✨ 27323

---
+++ baekjoon/27323.cpp
@@ -0,0 +1,18 @@
+// 직사각형
+// 사칙연산
+
+#include <iostream>
+
+int A, B;
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    std::cin >> A >> B;
+
+    std::cout << A * B << '\n';
+
+    return 0;
+}
