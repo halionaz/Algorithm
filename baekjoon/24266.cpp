da05b941dbeaf802a9a5abfcd9daf8c1c2a1828e
samhsj <samhsj@naver.com>
2023-08-26 02:22:50 -0400

    ✨ 24266

---
+++ baekjoon/24266.cpp
@@ -0,0 +1,18 @@
+// 알고리즘 수업 - 알고리즘의 수행 시간 5
+// 구현 & 수학 & 시뮬레이션
+
+#include <iostream>
+
+int main(){
+
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+
+    long long N;
+
+    std::cin >> N;
+
+    std::cout << N * N * N << '\n' << "3\n";
+
+    return 0;
+}
